1c5b31feb7542615aa6ef886f8f661a1de3931fc
sirlucasm <lucasmatheus2021@gmail.com>
2021-01-12 00:47:06 -0300

    added new video by hero id request and fixed some lib doc description
    Signed-off-by: sirlucasm <lucasmatheus2021@gmail.com>

--- src/Endpoints/Heroes.js
+++ src/Endpoints/Heroes.js
@@ -7,10 +7,15 @@ const { number } = require('superstruct');
 class Heroes {
 
 	/**
-	 * @constructor Heroes
-	 */
+ 		* @constructor Heroes
+	*/
 	constructor () {
 		this.getAll = this.#all.bind(this);
+		this.videos = {
+			by: {
+				id: this.#heroVideosById.bind(this),	
+			}
+		};
 		this.by = {
 			id: this.#heroID.bind(this),
 			name: this.#heroName.bind(this)
@@ -18,10 +23,10 @@ class Heroes {
 	}
 
 	/**
-	 * Returns all champions
-	 * @private
-	 * @returns { object }
-	 */
+ 	* Returns all champions
+		* @private
+		* @returns { object }
+	*/
 	async #all () {
 		try {
 			const { body } = await MAPIRequest(
@@ -34,12 +39,12 @@ class Heroes {
 	}
 
 	/**
-	 * Returns a champion by id
-	 * @private
-	 * @param {string | number} heroID - ID of champion
-	 * @returns { object }
-	 */
-	async #heroID (heroID = number) {
+ 	* Returns a champion by id
+		* @private
+		* @param {string | number} heroID - ID of champion
+		* @returns { object }
+	*/
+	async #heroID (heroID = Number) {
 		try {
 			const { body } = await MAPIRequest(
 				MAPIEnumHelper.createHeroUrl('detail', {
@@ -53,17 +58,54 @@ class Heroes {
 	}
 	
 	/**
-	 * Returns a hero by name
-	 * @private
-	 * @param { string } heroName - Name of hero
-	 * @returns { object }
-	 */
+ 	* Returns a hero by name
+		* @private
+		* @param { string } heroName - Name of hero
+		* @returns { object }
+	*/
 	async #heroName (heroName = String) {
 		try {
 			const body = await this.getAll();
 			const hero = body.data.filter(data => data.name === FormattersHelper.nameFirstCapitalLetter(heroName));
 			if (hero.length < 1) return new Error('No heroes with this name found');
 			return hero;
+		} catch (error) {
+			throw new Error(`An endpoint error ocurred with the code: ${error.code}`);
+		}
+	}
+
+	/**
+	* Returns videos by hero id
+		* @private
+		* @param {string | number} heroID - ID of champion
+		* @param {string | number} contentSize - Size of content requested
+		* @param {string | number} page - Number of pages
+		* @param { boolean } withPagination - If you want receive data with pagination
+		* @returns { object }
+	*/
+	async #heroVideosById (heroID = Number, withPagination = Boolean, contentSize = Number, page = Number) {
+		try {
+			let response;
+			if (withPagination) {
+				if (typeof page === 'function' || typeof contentSize === 'function') {
+					contentSize = 6;
+					page = 1;
+				}
+				response = await MAPIRequest(
+					MAPIEnumHelper.createHeroUrl('video', {
+						id: heroID,
+						size: contentSize,
+						page: page
+					})
+				);
+			} else {
+				response = await MAPIRequest(
+					MAPIEnumHelper.createHeroUrl('video', {
+						id: heroID,
+					})
+				);
+			}
+			return parseDataHelper(response.body);
 		} catch (error) {
 			throw new Error(`An endpoint error ocurred with the code: ${error.code}`);
 		}
